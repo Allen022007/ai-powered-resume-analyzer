e42983c6fc455f7262ddef03f35c06970552933b
0222007 <allenozario.rambo@gmail.com>
2025-07-21 20:26:01 +0530

    authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import {Link} from "react-router"
+
+const Navbar = () => {
+    return (
+        <div className={"navbar"}>
+            <Link to="/">
+                <p className={"text-2xl font-bold text-gradient"}>
+                    Resumai
+                </p>
+            </Link>
+            <Link to="/" className={"primary-button w-fit"}>
+                Upload Resume
+            </Link>
+        </div>
+    );
+};
+
+export default Navbar;
